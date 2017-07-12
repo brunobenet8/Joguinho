cf8dde7e5f3e16f68473fa21eef8d9b576b34b1a
brunobenet8 <bruno.rafaelbenet@gmail.com>
2017-07-12 12:17:37 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,5 +1,7 @@
 #faz o loop e imprime tabuada do 1 ao 9
 for i in range(1,10):
+    
     print("---------Tabuada do {} ---------".format(i))
     for j in range(1,11):
+        #imprime os numeros que serão multiplicados
         print("{} x {} = {}".format(i,j,i*j))
